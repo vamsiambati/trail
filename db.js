07c5be41113905831d86ad129aefcd02288e5e98
Vamsi Ambati <vamsiambati@Vamsis-MacBook-Air.local>
2019-03-12 00:15:46 +0530

    db2

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://vamsi:vamsi@employee-lm4k8.mongodb.net/test?retryWrites=true', { useNewUrlParser: true },(err)=>{
+mongoose.connect('mongodb+srv://vamsi:vamsi@employee-lm4k8.mongodb.net/employees?retryWrites=true', { useNewUrlParser: true },(err)=>{
     if(!err)
     console.log('DB conection sucess');
     else
