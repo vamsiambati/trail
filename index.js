7da831ed9fe5e7722fe87785daa11721f82931e6
Vamsi Ambati <vamsiambati@Vamsis-MacBook-Air.local>
2019-03-12 00:58:18 +0530

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app= express();
 app.use(bodyParser.json());
 app.use(cors()); 
 app.use('/', express.static('public/public'));
+
 app.listen(process.env.PORT || 3000, () => console.log('server started at port' + process.env.PORT));
 
 app.use('/employees', employeeClontroller)
